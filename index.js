5e32140a443ce4335ab24a1a087e8741b38e667a
Nestor <nestor.venegas95@gmail.com>
2021-04-10 21:07:08 -0500

    commit tree cdd086c343d00b64274da1f0537c9ad18f931ca7

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// Add Elements in the DOM
+
+// Add Elements to the DOM via innerHTML
+const newHeader = document.createElement('h1#victory')
+newHeader.innerHTML = 'YOUR-NAME is the champion'
+// This creates, with JavaScript, in the DOM, the quivalent of:
+
+// Change Properties on DOM Nodes
+
+// Remove Elements from the DOM
+const main = document.getElementById('main#main')
+body.remove(main)
+
